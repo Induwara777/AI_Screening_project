d9461eee40fb4c15929227b3602be8bc242c6cde
Induwara777 <induwaradilshan7@gmail.com>
2026-06-22 16:04:52 +0530

    06/22 First

---
+++ z_cv_json_creation.py
@@ -0,0 +1,49 @@
+import os
+import re
+from openai import OpenAI
+import fitz
+import z_ocr_fn
+import json
+import z_cv_extraction_set
+
+# Catching all names in a folder
+def file_name(folder):
+    names = [os.path.splitext(f)[0] for f in os.listdir(folder) if f.endswith(".pdf") ]
+    return names
+
+
+# Return json file for each CV
+def one_json(y):
+    text = z_ocr_fn.extraction(f"{y}.pdf")
+    file = z_cv_extraction_set.collection(text)
+    return file
+
+# Call json files for all CV.
+def json_file(output_folder):
+    try:
+
+        names_of_cv = file_name(output_folder)
+
+        os.makedirs(output_folder,exist_ok=True)
+        
+        for i,x in enumerate(names_of_cv,start = 1):
+            
+            json_path = os.path.join(output_folder,f"{x}.json")
+            if os.path.exists(json_path):
+                continue
+
+            llm_json = one_json(x)
+            llm_json["candidate_num"] = i
+
+            with open(f"cv_json_files\\{x}.json","w",encoding="utf-8") as f:
+                json.dump(llm_json,f,indent = 4)
+    except Exception as e:
+        print(f"Error : {e}")
+    
+if __name__ == "__main__":
+    json_file("cv_files")
+
+
+
+
+
